6ed6aee5ff1f3e47777e4d27f18effa49ef80d9e
jona <jona@jona.com>
2020-03-12 13:51:43 +0300

    ran test for the contact-us page

--- books/tests/tests_views.py
+++ books/tests/tests_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase,SimpleTestCase
 
 # Create your tests here.
 
+#test the homepage
 class HomePageTest(SimpleTestCase):
     def home_page_works_well(self):
         response=self.client.et('/')
@@ -9,7 +10,22 @@ class HomePageTest(SimpleTestCase):
         self.assertTemplateUsed(response,'books/index.html')
         self.assertContains(response,'Books Time')
 
+#test the about page
+class AboutPageTest(SimpleTestCase):
     def test_homepage_work(self):
         response=self.client.get('/about/')
         self.assertEqual(response.status_code,200)
         self.assertTemplateUsed(response,'books/about.html')
+
+#test the contact us page
+
+class ContactUsPageTest(TestCase):
+    def test_contact_us_page_works(self):
+        response=self.client.get('/contact-us/')
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'books/contact_form.html')
+        self.assertContains(response,'BookTime')
+        self.assertIsInstance(
+            response.context['form'],forms.ContactForm
+        )
+        
