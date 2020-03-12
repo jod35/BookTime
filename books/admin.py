71b56c6f9386312d4961ec98c21f2a9cfd909e81
jona <jona@jona.com>
2020-03-12 17:08:16 +0300

    simple customizations to the product admin

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import ProductTag,ProductImage,Product
+from django.utils.html import format_html
+
+#some customizations
+class ProductAdmin(admin.ModelAdmin):
+    list_display=('name','slug','in_stock','price')
+    list_filter=('active','in_stock','date_updated')
+    list_editable=('in_stock')
+    search_fields=('name',)
+    prepopulated_fields={'slug':"name"}
+
+
 
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product,ProductAdmin)
 admin.site.register(ProductImage)
 admin.site.register(ProductTag)
