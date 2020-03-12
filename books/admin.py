843bfbd5f25bba64055441cf047f5dd347b96c00
jona <jona@jona.com>
2020-03-12 17:18:12 +0300

    simple customizations to the product image admin

--- books/admin.py
+++ books/admin.py
@@ -10,9 +10,24 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields=('name',)
     prepopulated_fields={'slug':"name"}
 
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display=('thumbnail_tag','product_name')
+    readonly_fields=('thumbnails',)
+    search_fields=('product__name',)
+
+
+    def thumbnail_tag(self,obj):
+        return format_html("<img src="%">"%obj.thumbnail.url)
+        return "-"
+    
+    thumbnail_tag.short_description="Thumbnail"
+
+    def product_name(self,obj):
+        return obj.product.name
+
 
 
 # Register your models here.
 admin.site.register(Product,ProductAdmin)
-admin.site.register(ProductImage)
+admin.site.register(ProductImage,ProductImageAdmin)
 admin.site.register(ProductTag)
