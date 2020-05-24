3a9aa2ed817bc72bab69b32a2fa2d82013da6c34
jona <jona@jona.com>
2020-05-24 04:30:58 +0300

    set up the database and created a suoeruser

--- BookTime/settings.py
+++ BookTime/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bookstore.apps.BookstoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'BookTime.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'booktime',
+        'USER':'jona',
+        'HOST':'127.0.0.1',
+        'PASSWORD':'nathanoj35'
     }
 }
 
