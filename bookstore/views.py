df59f3dc65304593f2928006b5708d711133b6a6
jona <jona@jona.com>
2020-05-24 06:15:02 +0300

    created the booklist view and the book detail view

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Book
+from django.views.generic import ListView
 
-# Create your views here.
+
+class BookListView(ListView):
+    template_name='bookstore/index.html'
+    queryset=Book.objects.all()
+    paginate_by=5
+    context_object_name=books
+
+
+
+def book_details(request,title):
+    book=Book.objects.objects.filter(title=title).first()
+    context={
+        'book':book
+    }
+
+    return render(request,'bookstore/book_detail.html')
+
+
