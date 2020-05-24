bcbab2ca9d1e854f7c8e965605730dae9a071571
jona <jona@jona.com>
2020-05-24 04:22:42 +0300

    initial commit

--- books/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class BooksConfig(AppConfig):
-    name = 'books'
-
-    def ready(self):
-        from . import signals
--- books/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='books_home'),
-    path('about/',views.about,name='books_about'),
-    path('contact-us/',views.ContactUsView.as_view(),name='books_contact'),
-    path('products/',views.product_list,name="products")
-]
