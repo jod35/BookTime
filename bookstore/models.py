8ccc8216f20f5b01530994bbd9f9694611a06696
jod35 <jodestrevin@gmail.com>
2020-05-29 07:32:02 -0400

    added some tagging functionality

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from taggit.managers import TaggableManager
 
 class Book(models.Model):
+    tags=TaggableManager()
     title=models.CharField(max_length=40)
     author=models.CharField(max_length=25)
     written=models.DateField()
