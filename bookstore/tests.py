d5da9633383d805c99145a7dccbae4d79416c0e5
jod35 <jodestrevin@gmail.com>
2020-06-01 04:49:57 -0400

    tested homepage,login and register routes

--- bookstore/tests.py
+++ bookstore/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase,SimpleTestCase
+from django.test import Client
+# from .models import Book,Review
 
 # Create your tests here.
+
+class HomepageTestCase(TestCase):
+
+    def test_home_page_status_code(self):
+        response=self.client.get('/')
+        self.assertEqual(response.status_code,200)
+
+
+    def test_login_status_code(self):
+        response=self.client.get('/login/')
+        self.assertEqual(response.status_code,200)
+
+    
+    def test_signup_status_code(self):
+        response=self.client.get('/register/')
+        self.assertEqual(response.status_code,200)
+
